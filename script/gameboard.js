84118672e248f27b0f4b94423ac62d20b2a15337
Jens H. Nielsen <jens@jens.hk>
2017-02-20 15:13:06 +0800

    Jump with bug... fix it

--- script/gameboard.js
+++ script/gameboard.js
@@ -1,5 +1,5 @@
 ///////////// Gameboard ////////////////////
-
+/*
 var TILE = ?? // some distance in pixels
 var METER = TILE
 var FLOOR = { tilewidth , tileheight };
@@ -9,11 +9,113 @@ var GRAVITY = METER * 9.8 // can * a multiplyer
 var MAXDX = METER * 20 // max horz speed in tiles/sec
 var MAXDY = METER*20
 var JUMP = METER*5 //some jump distance
-
+*/
 /////////// View Window ///////////////////////
 
 
 
+var GameBoard = function(){
+
+    /*
+     *  Game global variables
+     */
+    var element = document.getElementById("gameboard");             // Gameboard element
+    var gravity = 5;                                               // Gravity in px;
+
+    /*
+     *  Game assets
+     */
+    var level = [];
+    var hero = new Hero(gravity);
+    var movement = {
+        "up": false,
+        "down": false,
+        "left": false,
+        "right": false,
+        "jump": false
+    };
+
+    /*
+     *  Event listeners
+     */
+    document.addEventListener('keydown', function(e) {
+
+        switch(e.keyCode){
+            case 38: // 87 - w key
+                movement.up = true;
+                break;
+            case 40: // 83 - s key
+                movement.down = true;
+                break;
+            case 37: // 65 - a key
+                movement.left = true;
+                break;
+            case 39: // 68 - d key
+                movement.right = true;
+                break;
+
+            case 32:
+                movement.jump = true;
+                break;
+            default:
+        }
+    });
+
+    document.addEventListener('keyup', function(e) {
+
+        switch(e.keyCode){
+            case 38: // 87 - w key
+                movement.up = false;
+                break;
+            case 40: // 83 - s key
+                movement.down = false;
+                break;
+            case 37: // 65 - a key
+                movement.left = false;
+                break;
+            case 39: // 68 - d key
+                movement.right = false;
+                break;
+
+            case 32:
+                movement.jump = false;
+                break;
+            default:
+        }
+    });
+
+
+    /*
+     *  Render
+     */
+    function render(){
+
+        hero.render(movement);
+
+    }
+
+    /*
+     *  Game loop
+     */
+    function animloop(){
+        window.requestAnimFrame(animloop);
+        render();
+    };
+    animloop();
+
+}
+
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+var gameBoard = new GameBoard();
 
 
 
--- script/hero.js
+++ script/hero.js
@@ -1,31 +1,128 @@
 //// Hero /////////
-var Hero = function(){
+var Hero = function(gravity){
+
+
+    console.log("Hero gravity", gravity)
+
+    this.health = 100;                          // Player health
+    this.height = 100;
+    this.width = 50;
+
+    this.gravityInPixel = gravity
 
     this.position = {
         'x': window.innerWidth / 2,
-        'y': window.innerHeight / 2,
-        'width': 0,			// Hero width 1 METER
-        'height': 0,		// Hero height 2 METER
-        'hp': 3
+        'y': window.innerHeight / 2
     }
 
-    this.speed = 20;
+    var speed = 20;                             // Speed
+    var currentSpeed = 0;
+    var element = document.getElementById("hero");
+    var self = this;
+
+    /*
+     * Jump variables
+     */
+    var jumpSpeed = 20;
+    var maxJumpHeight = 300;
+    var currentJumpHeight = 0;
+    var isJumping = false;
+    var jumpingUp = true;
+
+
+    function addGravity(){
+        self.position.y += self.gravityInPixel;
+
+        //console.log(self.position.y);
+        var bottom = window.innerHeight - 90;
+
+        if(self.position.y > bottom){
+            self.position.y = bottom;
+        }
+    }
+
 
     this.kill = function(){
         console.log('bang');
     }
 
-    this.jump = function(){
+    this.jump = function(movement){
+
+
+
+        if(!movement.jump && !isJumping){   // if space is not press and the hero is not currently jumping
+            return
+        }
+
+        isJumping = true;
+
+
+
+        if(currentJumpHeight < maxJumpHeight && isJumping && jumpingUp){
+            currentJumpHeight += jumpSpeed;
+            this.position.y -= jumpSpeed;
+            console.log(currentJumpHeight);
+        }
+
+
+        if(currentJumpHeight >= maxJumpHeight && isJumping){
+            currentJumpHeight -= jumpSpeed;
+            this.position.y += jumpSpeed;
+            console.log(currentJumpHeight);
+            jumpingUp = false;
+        }
+
+        if(currentJumpHeight < maxJumpHeight && isJumping && !jumpingUp){
+
+            if( currentJumpHeight <= speed){ // close to an object than the speed
+                currentJumpHeight = 0;
+               // this.position.y += currentJumpHeight;
+               // debugger;
+            }else{
+                currentJumpHeight -= jumpSpeed;
+                this.position.y += jumpSpeed;
+            }
+
+            console.log(currentJumpHeight);
+        }
+
+        if(currentJumpHeight <= 0){
+            isJumping = false;
+            jumpingUp = true;
+            console.log(currentJumpHeight);
+        }
+
+        if(movement.jump && !isJumping){
+            this.position.y -= jumpSpeed;
+            currentJumpHeight += jumpSpeed;
+            console.log(currentJumpHeight);
+        }
 
     }
 
-    this.run = function(){
+    this.run = function(movement){
+
+        if(movement.left){
+            this.position.x -= speed;
+        }
+
+        if(movement.right){
+            this.position.x += speed;
+        }
+
 
     }
-
 
     // Hero render
-    this.render = function(){
+    this.render = function(movement){
+
+
+        this.run(movement);
+        addGravity();
+        this.jump(movement);
+
+        element.style.top = this.position.y;
+        element.style.left = this.position.x;
 
     }
 
